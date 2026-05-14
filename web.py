335ac6ba38b309a06dd2be502e8a0401a824bf12
JinC1102 <justin20051102@gmail.com>
2026-05-14 11:07:37 +0800

    5/14

--- web.py
+++ web.py
@@ -43,23 +43,17 @@ def index():
     link += "<a href=/rate>本週新片進DB</a><hr>"
 
     return link
+
 @app.route("/webhook", methods=["POST"])
-
 def webhook():
+    # build a request object
+    req = request.get_json(force=True)
+    # fetch queryResult from json
+    action = req.get("queryResult").get("action")
+    msg = req.get("queryResult").get("queryText")
+    info = "動作：" + action + "； 查詢內容：" + msg
 
-# build a request object
-
-req = request.get_json(force=True)
-
-# fetch queryResult from json
-
-action = req.get("queryResult").get("action")
-
-msg = req.get("queryResult").get("queryText")
-
-info = "動作：" + action + "； 查詢內容：" + msg
-
-return make_response(jsonify({"fulfillmentText": info}))
+    return make_response(jsonify({"fulfillmentText": info}))
 
 @app.route("/rate")
 def rate():
